7995c801bc916fb4a3361464926fd695795be9dd
Omkar P. More <Omkar P. More@Snow>
2022-02-23 01:38:51 +0530

    all files commit

---
+++ springboot-hibernate-one-many-mapping/src/main/java/om/nimap/demo/entity/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package om.nimap.demo.entity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import om.nimap.demo.entity.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long>{
+
+}
